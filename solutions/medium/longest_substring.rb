0437944e6c83095bf77140284632d31d2b155d2f
Babygroot <it3rnity@gmail.com>
2025-07-22 00:37:19 +0100

    feat(solutions): Solved longest substring

--- solutions/medium/longest_substring.rb
+++ solutions/medium/longest_substring.rb
@@ -1,23 +1,22 @@
 # @param {String} s
 # @return {Integer}
+
 def length_of_longest_substring(s)
-  # s = "abcabcbb"
   longest_substring = 0
   current_longest_substring = 0
   chars_matched = {}
-  s.split.each do |char|
-    if chars_matched["char"]
-      current_longest_substring = 1
-      chars_matched = {}
-    else
+  s.each_char do |char|
+    if chars_matched[char].nil?
       current_longest_substring += 1
-      chars_matched["char"] = 1
+      chars_matched[char] = 1
+    else
+      current_longest_substring = 0
+      chars_matched = {}
     end
 
     if current_longest_substring > longest_substring
       longest_substring = current_longest_substring
     end
   end
-
   longest_substring
 end
